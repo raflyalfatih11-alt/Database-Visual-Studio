33d555c252a4a64aa25fcf655ad45d23cdd693a4
User <User@CAI>
2025-12-03 15:09:54 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        ComboBox1 = New ComboBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(122, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(34, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "NPM"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(122, 87)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(39, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Nama"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(122, 126)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(78, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Jenis Kelamin"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        Button1.Location = New Point(147, 161)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(69, 38)
+        Button1.TabIndex = 3
+        Button1.Text = "<<"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(223, 52)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(218, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(223, 87)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(218, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(223, 123)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(218, 23)
+        ComboBox1.TabIndex = 6
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        Button2.Location = New Point(222, 161)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(69, 38)
+        Button2.TabIndex = 7
+        Button2.Text = "<"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        Button3.Location = New Point(297, 161)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(69, 38)
+        Button3.TabIndex = 8
+        Button3.Text = ">"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        Button4.Location = New Point(372, 161)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(69, 38)
+        Button4.TabIndex = 9
+        Button4.Text = ">>"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(470, 82)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(86, 35)
+        Button5.TabIndex = 10
+        Button5.Text = "Tambah"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(470, 123)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(86, 35)
+        Button6.TabIndex = 11
+        Button6.Text = "Hapus"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(470, 164)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(86, 35)
+        Button7.TabIndex = 12
+        Button7.Text = "Simpan"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(102, 236)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(240, 150)
+        DataGridView1.TabIndex = 13
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports Microsoft.Data.SqlClient
+
+Imports System.Data
+Public Class Form1
+    Private MyDatAdp As SqlDataAdapter
+    Private MyCmdBld As SqlCommandBuilder
+    Private MyDataTbl As New DataTable
+    Private MyCn As New SqlConnection()
+    Private MyRowPosition As Integer = 0
+    Private strJK As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' JANGAN panggil FormClosed() manual
+        'Me.FormClosed()
+
+        MyCn.ConnectionString =
+            "Data Source=(localdb)\MSSQLLocalDB;" &
+             "AttachDbFilename=C:\Users\User\source\repos\database visual\Database1.mdf;" &
+             "Integrated Security=True;"
+
+
+        MyCn.Open()
+
+        MyDatAdp = New SqlDataAdapter("SELECT * FROM dbo.mahasiswa", MyCn)
+        MyCmdBld = New SqlCommandBuilder(MyDatAdp)
+        MyDatAdp.Fill(MyDataTbl)
+
+        If MyDataTbl.Rows.Count > 0 Then
+            Dim MyDataRow As DataRow = MyDataTbl.Rows(0)
+
+            TextBox1.Text = MyDataRow("NPM").ToString()
+            TextBox2.Text = MyDataRow("Nama").ToString()
+            ComboBox1.Text = MyDataRow("JK").ToString()
+        End If
+
+        showRecords()
+
+    End Sub
+
+    Private Sub showRecords()
+        If MyDataTbl.Rows.Count = 0 Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            ComboBox1.Text = ""
+            Exit Sub
+        End If
+
+        TextBox1.Text = MyDataTbl.Rows(MyRowPosition)("NPM").ToString()
+        TextBox2.Text = MyDataTbl.Rows(MyRowPosition)("Nama").ToString()
+        ComboBox1.Text = MyDataTbl.Rows(MyRowPosition)("JK").ToString()
+    End Sub
+
+    ' FIRST RECORD
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MyRowPosition = 0
+        showRecords()
+    End Sub
+
+    ' PREVIOUS RECORD
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If MyRowPosition > 0 Then
+            MyRowPosition -= 1
+            showRecords()
+        End If
+    End Sub
+
+    ' NEXT RECORD
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If MyRowPosition < MyDataTbl.Rows.Count - 1 Then
+            MyRowPosition += 1
+            showRecords()
+        End If
+    End Sub
+
+    ' LAST RECORD
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If MyDataTbl.Rows.Count > 0 Then
+            MyRowPosition = MyDataTbl.Rows.Count - 1
+            showRecords()
+        End If
+    End Sub
+
+    ' ADD NEW
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim MyNewRow As DataRow = MyDataTbl.NewRow()
+        MyDataTbl.Rows.Add(MyNewRow)
+        MyRowPosition = MyDataTbl.Rows.Count - 1
+        showRecords()
+    End Sub
+
+    ' DELETE
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If MyDataTbl.Rows.Count > 0 Then
+            MyDataTbl.Rows(MyRowPosition).Delete()
+            MyDatAdp.Update(MyDataTbl)
+            MyRowPosition = 0
+            showRecords()
+        End If
+    End Sub
+
+    ' UPDATE
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If MyDataTbl.Rows.Count > 0 Then
+            MyDataTbl.Rows(MyRowPosition)("NPM") = TextBox1.Text
+            MyDataTbl.Rows(MyRowPosition)("Nama") = TextBox2.Text
+            MyDataTbl.Rows(MyRowPosition)("JK") = ComboBox1.Text
+            MyDatAdp.Update(MyDataTbl)
+        End If
+
+        If TextBox1.Text.Trim() = "" Then
+            MessageBox.Show("NPM wajib diisi!", "Validasi Data", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub ' Keluar dari sub sebelum mencoba Update
+        End If
+
+        If MyDataTbl.Rows.Count > 0 Then
+            MyDataTbl.Rows(MyRowPosition)("NPM") = TextBox1.Text
+            ' ... kode update lainnya
+            MyDatAdp.Update(MyDataTbl)
+        End If
+
+    End Sub
+
+End Class
